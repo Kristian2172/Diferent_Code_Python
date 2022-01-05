4cf9caae833bdf4b358a522ee553a7086883276d
Cristian Grigorita <cristian.grigorita.2005@gmail.com>
2022-01-05 17:30:15 +0000

    Work with functions 'def'

--- Practise_Exercises.py
+++ Practise_Exercises.py
@@ -1,30 +1,59 @@
-greeting = "hello, world"
-chars = []
-
-for x in greeting:
-    chars.append(x)
-
-print(chars)
-
-# O resultado destes dois codigos é o mesmo mas a estrutura é diferente
-
-chars = [i for i in greeting]
-print(chars)
-
-# Outros exemplos onde podemos usar o list comprehension
-numbers = [n for n in range(0, 11)]
-print(numbers)
-
-numbers = [n * n for n in range(0, 11)]
-print(numbers)
-
-# Mostra a multiplicacao dos numeros impares
-numbers = [n * n for n in range(0, 11) if n % 2 != 0]
-print(numbers)
-
-# Conversão de centimentros para polegadas
-len_in_centi = [12, 10, 54, 124, 64]
-len_in_inches = [(round(cm / 2.54, 2)) for cm in len_in_centi]
-print(len_in_inches)
+def greeting():
+    print("Hello World")
 
 
+def print_name(name):
+    print(name)
+
+
+print_name("John")
+
+
+def get_sum(a, b):
+    return a + b
+
+
+result = get_sum(10, 2)
+print(result)
+
+
+def is_adult(age):
+    return age >= 18
+
+
+is_adult = is_adult(20)
+print(is_adult)
+
+
+def is_palindrome(text):
+    return text == text[::-1]
+
+
+is_palindrome("aabaa")
+is_palindrome("aabba")
+
+
+def calc_taxes(p1, p2, p3):
+    return sum((p1, p2, p3)) * 0.06
+
+
+calc_taxes(10, 20, 30)
+
+
+# Para receber inumeros argumentos temos de meter *args
+def calc_taxes(*args):
+    for x in args:
+        print(f"Got payment = {x}")
+    return sum(args) * 0.06
+
+
+calc_taxes(10, 20, 30, 40)
+
+
+# Para receber inumeros argumentos que contêm key values pairs devemos de meter **kwargs
+def save_players(**kwargs):
+    for k, v in kwargs.items():
+        print(f"Player {k} has rating {v}")
+
+
+save_players(Carlsen=2800, Giri=2780)
