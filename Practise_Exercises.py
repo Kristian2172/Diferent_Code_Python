33450e6984d9f6d0d864be5ffa8cf06718cfc89c
Cristian Grigorita <cristian.grigorita.2005@gmail.com>
2022-01-19 16:38:22 +0000

    Work with Attributes and Methods

--- Practise_Exercises.py
+++ Practise_Exercises.py
@@ -1,13 +1,28 @@
 class Character:
+    # Atributos
+    max_speed = 100
+    dead_health = 0
 
     def __init__(self, race, damage=10, armor=20):
         # Criação dos atributos
         self.race = race
         self.damage = damage
         self.armor = armor
+        self.health = 100
+
+    def hit(self, damage):
+        self.health -= damage
+
+    def is_dead(self):
+        return self.health == Character.dead_health
 
 
-unit = Character("Elf", 20, 40)
+unit = Character("Ork")
 
-print(unit.damage)
-print(unit.armor)
+unit.hit(20)
+print(unit.health)
+
+print(unit.is_dead())
+
+unit.hit(80)
+print(unit.is_dead())
