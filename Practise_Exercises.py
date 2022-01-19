823405e6d05e94fa772a5e5fd4b6a6407405ca37
Cristian Grigorita <cristian.grigorita.2005@gmail.com>
2022-01-19 17:32:18 +0000

    Work with Constants, Protected and private attributes and Properties

--- Practise_Exercises.py
+++ Practise_Exercises.py
@@ -1,28 +1,52 @@
 class Character:
-    # Atributos
-    max_speed = 100
-    dead_health = 0
+    # Constants
+    MAX_SPEED = 100
 
-    def __init__(self, race, damage=10, armor=20):
-        # Criação dos atributos
-        self.race = race
+    def __init__(self, race, damage=10):
         self.damage = damage
-        self.armor = armor
-        self.health = 100
+        # private
+        self.__race = race
+        # semi-privados - não se vêm mas podem ser modificados
+        self._health = 100
+
+        self._current_speed = 20
 
     def hit(self, damage):
-        self.health -= damage
+        self._health -= damage
 
-    def is_dead(self):
-        return self.health == Character.dead_health
+    # @property dá permissão para ser lido os atributos privados e semi-privados
+    @property
+    def health(self):
+        return self._health
+
+    @property
+    def race(self):
+        return self.__race
+
+    @property
+    def current_speed(self):
+        return self._current_speed
+
+    # Propriedade com a possibilidade de ser introduzido informação
+    @current_speed.setter
+    def current_speed(self, current_speed):
+        if current_speed < 0:
+            self._current_speed = 0
+        elif current_speed > 100:
+            self._current_speed = 100
+        else:
+            self._current_speed = current_speed
 
 
-unit = Character("Ork")
+c = Character("Elf")
 
-unit.hit(20)
-print(unit.health)
+print(c.health)
+print(c.race)
 
-print(unit.is_dead())
-
-unit.hit(80)
-print(unit.is_dead())
+print(c.current_speed)
+c.current_speed = 50
+print(c.current_speed)
+c.current_speed = 1000
+print(c.current_speed)
+c.current_speed = -10
+print(c.current_speed)
